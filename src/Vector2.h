c941b62cce2adb26bc842cd94a89d64f652dcaf6
AVIX <avix256@gmail.com>
2020-09-22 22:46:05 +0700

    ✅

--- src/Vector2.h
+++ src/Vector2.h
@@ -8,7 +8,6 @@
 
 class Vector2
 {
-
 public:
     int x;
     int y;
@@ -29,20 +28,27 @@ public:
         y = _y;
         color = _color;
     }
+    
+    void Init(int _x, int _y, Color _color)
+    {
+        x = _x;
+        y = _y;
+        color = _color;
+    }
 
     void setColor(Color _color) {
         color = _color;
     }
 
-    void Init(int x, int y, Color color)
-    {
-        Vector2(x,y,color);
-    }
-
     void Read()
     {
-        printf("Enter x and y: ");
-        scanf("%d%d", &x, &y);
+        while (
+            printf("Enter x and y: "),
+            fflush(stdin),
+            2 != scanf("%d%d", &x, &y) &&
+            printf("[!] You have entered incorrect data, please try again.\n")
+        );
+        fflush(stdin);
     }
 
     double Length()
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <ctime>
+#include <Windows.h>
 
 #include "SDL2/SDL.h"
 #include "Window.h"
@@ -10,57 +11,85 @@
 
 int Script(void *data)
 {
+    SetConsoleOutputCP(65001);
     Window *window = (Window *)data;
     window->Clear();
-    srand((unsigned) time(0));
+    srand((unsigned)time(0));
 
     //4) В main продемонстрировать работу со статическими объектами вашего класса;
+    printf("4) В main продемонстрировать работу со статическими объектами вашего класса;\n");
     Vector2 red(5, 4, Color(255, 0, 0));
     window->Draw(&red);
+
     system("pause");
     system("cls");
     window->Clear();
 
     //5) В main продемонстрировать работу с динамическими объектами вашего класса;
+    printf("5) В main продемонстрировать работу с динамическими объектами вашего класса;\n");
     Vector2 *green = new Vector2(-2, 2, Color(0, 255, 0));
     window->Draw(green);
+
     system("pause");
     system("cls");
     window->Clear();
+    delete green;
 
     //6.1) В main продемонстрировать работу с динамическим массивом объектов вашего класса;
+    printf("6.1) В main продемонстрировать работу с динамическим массивом объектов вашего класса;\n");
     int n = 10;
-    Vector2 * ptr = (Vector2 *)malloc(sizeof(Vector2) * n);
-    for (int i = 0; i< n; i++)
-        *(ptr+i) = Vector2(
+    Vector2 *ptr = (Vector2 *)malloc(sizeof(Vector2) * n);
+    for (int i = 0; i < n; i++)
+        *(ptr + i) = Vector2(
             rand() % 20 - 10,
             rand() % 20 - 10,
-            Color(rand() % 255,rand() % 255,rand() % 255)
-        );
+            Color(rand() % 255, rand() % 255, rand() % 255));
 
-    for (int i = 0; i< n; i++)
-        window->Draw(ptr+i);
-    
+    for (int i = 0; i < n; i++)
+    {
+        window->Draw(ptr + i);
+        (ptr + i)->Display();
+    }
+
     system("pause");
     system("cls");
     window->Clear();
+    free(ptr);
 
     //6.2) В main продемонстрировать работу с массивом динамических объектов вашего класса;
-    Vector2* arr[10];
-    for (int i = 0; i < 10; i++)
+    printf("6.2) В main продемонстрировать работу с массивом динамических объектов вашего класса;\n");
+    int k = 10;
+    Vector2 **arr = new Vector2 *[k];
+    for (int i = 0; i < k; i++)
         arr[i] = new Vector2(
             rand() % 20 - 10,
             rand() % 20 - 10,
-            Color(rand() % 255,rand() % 255,rand() % 255)
-        );
+            Color(rand() % 255, rand() % 255, rand() % 255));
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < k; i++)
+    {
         window->Draw(arr[i]);
-    
+        arr[i]->Display();
+    }
+
     system("pause");
     system("cls");
     window->Clear();
+    delete[] arr;
 
+    //7) Продемострировать применение операторов ✅new, ✅delete, ✅delete[];
+    //8) Продемострировать применение функций calloc, ✅malloc, realloc, ✅free;
+    printf("8) Продемострировать применение функций calloc, ✅malloc, realloc, ✅free;\n");
+    Vector2 *callocated = (Vector2 *)calloc(10, sizeof(Vector2));
+    callocated[7].Init(1, 2, Color(255, 255, 255));
+    Vector2 *reallocated = (Vector2 *)realloc(callocated, 10 * sizeof(Vector2));
+    window->Draw(&reallocated[7]);
+
+    system("pause");
+    system("cls");
+    window->Clear();
+    free(callocated);
+    free(reallocated);
 
     while (1)
     {
@@ -68,14 +97,14 @@ int Script(void *data)
 
         Vector2 v1;
         v1.Read();
-        v1.setColor(Color(0,255,0));
+        v1.setColor(Color(0, 255, 0));
         printf("v1 = ");
         v1.Display();
         window->Draw(&v1);
 
         Vector2 v2;
         v2.Read();
-        v2.setColor(Color(0,255,0));
+        v2.setColor(Color(0, 255, 0));
         printf("v2 = ");
         v2.Display();
         window->Draw(&v2);
